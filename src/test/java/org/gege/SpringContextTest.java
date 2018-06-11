67d1b178d33bd7a432f4bef3cf1268affda41a96
jige <jige@norteksoft.com>
2018-06-11 16:43:01 +0800

    纯净的项目

--- src/test/java/org/gege/SpringContextTest.java
+++ src/test/java/org/gege/SpringContextTest.java
@@ -1,26 +1,14 @@
 package org.gege;
 
-import java.util.List;
-
-import org.gege.dao.Dao;
-import org.gege.entity.Emp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("/spring.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SpringContextTest {
-	@Autowired
-	private Dao dao;
 	@Test
 	public void test() {
-		List<? extends Object> listObj = dao.listObj(Emp.class);
-		for (Object object : listObj) {
-			Emp emp = (Emp)object;
-			System.out.println(emp.toString());
-		}
 	}
 
 }
--- src/test/java/org/gege/dao/DaoTest.java
+++ src/test/java/org/gege/dao/DaoTest.java
@@ -1,30 +1,15 @@
 package org.gege.dao;
 
-import org.gege.entity.Emp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @ContextConfiguration("/spring.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DaoTest {
-	@Autowired
-	private Dao dao;
-//	@Autowired
-//	private Service services;
 	@Test
 	public void test1(){
-		
-		Emp emp =null;
-		try {
-			emp = dao.findObject("from Emp  where name = ? ",new Object[]{"ss"},new BeanPropertyRowMapper<Emp>(Emp.class));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println(emp);
 	}
 }
 
